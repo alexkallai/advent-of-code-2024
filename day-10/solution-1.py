9b7458127c1fec1d13ec63f7b3900d0bb2908e69
alexkallai <scythe1092@gmail.com>
2024-12-11 19:54:29 +0100

    Day 10 sol 1

--- day-10/solution-1.py
+++ day-10/solution-1.py
@@ -9,7 +9,7 @@ with open(file_arg, "r", encoding = "utf-8") as f:
     lines = [(line.strip()) for line in f.readlines()]
 
 for index, line in enumerate(lines):
-    lines[index] = [int(a) for a in list(lines[index].strip())]
+    lines[index] = [int(a) if a.isalnum() else ' ' for a in list(lines[index].strip())]
 
 width = len(lines[0])
 height = len(lines)
@@ -18,15 +18,23 @@ dirs = [(-1, 0),
         (1, 0),
         (0, -1)]
 
-trailhead_score_sum = 0
-trailhead_scores = []
+def debug_print(current_list):
+    work_copy = deepcopy(lines)
+    for coord in current_list:
+        work_copy[coord[0]][coord[1]] = "X"
+    for line in work_copy:
+        print([str(i) for i in line])
+    print("\n")
+
+available_heads = {}
 def search_next_recur(current_list: list):
     global lines
-    global trailhead_score_sum
     global dirs
 
     curr_value = lines[current_list[-1][0]][current_list[-1][1]]
 
+    start_coord = current_list[0]
+    #debug_print(current_list)
     for dir in dirs:
         nc = (current_list[-1][0] + dir[0], current_list[-1][1] + dir[1]) 
         if 0 <= nc[0] < height and 0 <= nc[1] < width:
@@ -35,8 +43,11 @@ def search_next_recur(current_list: list):
                 next_value = lines[nc[0]][nc[1]]
                 if next_value == curr_value + 1:
                     if next_value == 9:
-                        trailhead_score_sum += 1
-                        trailhead_scores[-1].append(1)
+                        if start_coord in available_heads:
+                            available_heads[start_coord].append(nc)
+                        else:
+                            available_heads.update({start_coord: [nc]})
+                        
                         continue
                     search_next_recur(current_list + [(nc[0], nc[1])])
 
@@ -45,10 +56,12 @@ for i, line in enumerate(lines):
     for j, no in enumerate(line):
         if lines[i][j] == 0:
             coord = (i, j)
-            trailhead_scores.append([])
-            trailhead_scores[-1].append((i,j))
             search_next_recur([(i, j)])
 
+
 # 640 too high
-print(trailhead_score_sum)
+sum = 0
+for key in available_heads.keys():
+    sum += len(set(available_heads[key]))
+print(sum)
 pass
