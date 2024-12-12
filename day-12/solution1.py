b6ede34351f00b975abb9af0e5ef1fa9a461bff7
Alex Kallai <kallaia@dmgmori.com>
2024-12-12 08:09:11 +0100

    Day 12 pt1 done

--- day-12/solution1.py
+++ day-12/solution1.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+from collections import deque
 
 file_folder_path = Path(__file__).parent
 file_arg = os.path.join(file_folder_path, "input.txt")
@@ -9,40 +10,42 @@ with open(file_arg, "r", encoding = "utf-8") as f:
 
 width = len(lines[0])
 height = len(lines)
-dirs = [(-1, 0),
-        (0, 1),
-        (1, 0),
-        (0, -1)]
-processed_coords = []
+dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+processed_coords = set() 
 list_of_gardens = []
 
-def collect_plots_recur(current_list: list):
 
-    curr_value = lines[current_list[-1][0]][current_list[-1][1]]
-    processed_coords.append(current_list[-1])
+def collect_plots_iterative(start_coord):
+    queue = deque([start_coord])  
+    curr_value = lines[start_coord[0]][start_coord[1]]
+    garden = []
 
-    for dir in dirs:
-        nc = (current_list[-1][0] + dir[0], current_list[-1][1] + dir[1]) 
-        if 0 <= nc[0] < height and 0 <= nc[1] < width:
-            if (nc[0], nc[1]) not in processed_coords:
-                # if next coord is current + 1
-                next_value = lines[nc[0]][nc[1]]
-                if next_value == curr_value:
-                    return collect_plots_recur(current_list + [(nc[0], nc[1])])
-    return current_list
+    while queue:
+        coord = queue.popleft()
+        if coord in processed_coords:
+            continue
+
+        processed_coords.add(coord) 
+        garden.append(coord)
+
+        for dir in dirs:
+            nc = (coord[0] + dir[0], coord[1] + dir[1])
+            if (
+                0 <= nc[0] < height
+                and 0 <= nc[1] < width
+                and nc not in processed_coords
+                and lines[nc[0]][nc[1]] == curr_value
+            ):
+                queue.append(nc)
+    return garden
+
 
 for i, line in enumerate(lines):
     for j, char in enumerate(line):
         coord = (i, j)
         if coord not in processed_coords:
-            garden_elems = collect_plots_recur([coord])
+            garden_elems = collect_plots_iterative(coord)
             list_of_gardens.append(garden_elems)
-            pass
-
-#for garden in list_of_gardens:
-    #for coord in garden:
-        #print(lines[coord[0]][coord[1]], end="")
-    #print("\n")
 
 def get_number_of_neighbours(garden, coord):
     sum = 0
